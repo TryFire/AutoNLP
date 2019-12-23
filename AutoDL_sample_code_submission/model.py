ef74424ee5298ce7e0c73839ed4d15cfc349001b
TryFire <TryFire@users.noreply.github.com>
2019-12-23 04:13:26 +0100

    model

--- AutoDL_sample_code_submission/model.py
+++ AutoDL_sample_code_submission/model.py
@@ -2,11 +2,11 @@
 
 import pandas as pd
 import os
+import re
 import argparse
 import time
 import jieba
 import pickle
-import re
 import tensorflow as tf
 import numpy as np
 import sys, getopt
@@ -15,11 +15,8 @@ from tensorflow.python.keras import models
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.layers import Dropout
 from tensorflow.python.keras.layers import Embedding
-from tensorflow.python.keras.layers import SeparableConv1D
-from tensorflow.python.keras.layers import MaxPooling1D
-from tensorflow.python.keras.layers import MaxPooling2D
-from tensorflow.python.keras.layers import Flatten
-from tensorflow.python.keras.layers import GlobalAveragePooling1D
+from tensorflow.python.keras.layers import SpatialDropout1D
+from tensorflow.python.keras.layers import LSTM
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -29,16 +26,24 @@ from sklearn.feature_selection import f_classif
 from tensorflow.python.keras.preprocessing import text
 from tensorflow.python.keras.preprocessing import sequence
 
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+config.log_device_placement = True  # to log device placement (on which device the operation ran)
+                                    # (nothing gets printed in Jupyter, only if you run it standalone)
+sess = tf.Session(config=config)
+set_session(sess)  # set this TensorFlow session as the default session for Keras
 
-MAX_VOCAB_SIZE = 10000
 
+MAX_SEQ_LENGTH = 500
+MAX_VOCAB_SIZE = 20000 # Limit on the number of features. We use the top 20K features
 
 # code form https://towardsdatascience.com/multi-class-text-classification-with-lstm-1590bee1bd17
 def clean_en_text(dat):
-
+    
     REPLACE_BY_SPACE_RE = re.compile('["/(){}\[\]\|@,;]')
     BAD_SYMBOLS_RE = re.compile('[^0-9a-zA-Z #+_]')
-
+    
     ret = []
     for line in dat:
         # text = text.lower() # lowercase text
@@ -50,7 +55,7 @@ def clean_en_text(dat):
 
 def clean_zh_text(dat):
     REPLACE_BY_SPACE_RE = re.compile('[“”【】/（）：！～「」、|，；。"/(){}\[\]\|@,\.;]')
-
+    
     ret = []
     for line in dat:
         line = REPLACE_BY_SPACE_RE.sub(' ', line)
@@ -59,12 +64,80 @@ def clean_zh_text(dat):
     return ret
 
 
+def sequentialize_data(train_contents, val_contents=None):
+    """Vectorize data into ngram vectors.
+
+    Args:
+        train_contents: training instances
+        val_contents: validation instances
+        y_train: labels of train data.
+
+    Returns:
+        sparse ngram vectors of train, valid text inputs.
+    """
+    tokenizer = text.Tokenizer(num_words = MAX_VOCAB_SIZE)
+    tokenizer.fit_on_texts(train_contents)
+    x_train = tokenizer.texts_to_sequences(train_contents)
+
+    if val_contents:
+        x_val = tokenizer.texts_to_sequences(val_contents)
+
+    max_length = len(max(x_train, key=len))
+    if max_length > MAX_SEQ_LENGTH:
+        max_length = MAX_SEQ_LENGTH
+
+    x_train = sequence.pad_sequences(x_train, maxlen=max_length)
+    if val_contents:
+        x_val = sequence.pad_sequences(x_val, maxlen=max_length)
+
+    word_index = tokenizer.word_index
+    num_features = min(len(word_index) + 1, MAX_VOCAB_SIZE)
+    if val_contents:
+        return x_train, x_val, word_index, num_features, tokenizer, max_length
+    else:
+        return x_train, word_index, num_features, tokenizer, max_length
+
+
+def _get_last_layer_units_and_activation(num_classes):
+    """Gets the # units and activation function for the last network layer.
+
+    Args:
+        num_classes: Number of classes.
+
+    Returns:
+        units, activation values.
+    """
+    if num_classes == 2:
+        activation = 'sigmoid'
+        units = 1
+    else:
+        activation = 'softmax'
+        units = num_classes
+    return units, activation
+
+
+def sep_cnn_model(input_shape,
+                  num_classes,
+                  num_features,
+                  blocks=1,
+                  filters=64,
+                  kernel_size=4,
+                  dropout_rate=0.25):
+    op_units, op_activation = _get_last_layer_units_and_activation(num_classes)
+
+    model = models.Sequential()
+    model.add(Embedding(input_dim=num_features, output_dim=100, input_length=input_shape))
+    model.add(SpatialDropout1D(0.2))
+    model.add(LSTM(50, dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(op_units, activation=op_activation))
+    return model
+
+
 def _tokenize_chinese_words(text):
     return ' '.join(jieba.cut(text, cut_all=False))
 
-
 def vectorize_data(x_train, x_val=None):
-    vectorizer = TfidfVectorizer(ngram_range=(1, 1), max_features = MAX_VOCAB_SIZE)
+    vectorizer = TfidfVectorizer(ngram_range=(1, 1))
     if x_val:
         full_text = x_train + x_val
     else:
@@ -83,9 +156,12 @@ def ohe2cat(label):
 
 
 class Model(object):
-    """ Example of valid model """
+    """ 
+        model of CNN baseline without pretraining.
+        see `https://aclweb.org/anthology/D14-1181` for more information.
+    """
 
-    def __init__(self, metadata):
+    def __init__(self, metadata, train_output_path="./", test_input_path="./"):
         """ Initialization for model
         :param metadata: a dict formed like:
             {"class_num": 10,
@@ -96,37 +172,19 @@ class Model(object):
         """
         self.done_training = False
         self.metadata = metadata
-        self.train_output_path = './'
-        self.test_input_path = './'
+        self.train_output_path = train_output_path
+        self.test_input_path = test_input_path
 
     def train(self, train_dataset, remaining_time_budget=None):
-        """Train this algorithm on the NLP task.
-
-         This method will be called REPEATEDLY during the whole training/predicting
-         process. So your `train` method should be able to handle repeated calls and
-         hopefully improve your model performance after each call.
-
-         ****************************************************************************
-         ****************************************************************************
-         IMPORTANT: the loop of calling `train` and `test` will only run if
-             self.done_training = False
-           (the corresponding code can be found in ingestion.py, search
-           'M.done_training')
-           Otherwise, the loop will go on until the time budget is used up. Please
-           pay attention to set self.done_training = True when you think the model is
-           converged or when there is not enough time for next round of training.
-         ****************************************************************************
-         ****************************************************************************
-
+        """model training on train_dataset.
+        
         :param train_dataset: tuple, (x_train, y_train)
-            x_train: list of str, input training sentence.
+            x_train: list of str, input training sentences.
             y_train: A `numpy.ndarray` matrix of shape (sample_count, class_num).
                      here `sample_count` is the number of examples in this dataset as train
                      set and `class_num` is the same as the class_num in metadata. The
                      values should be binary.
         :param remaining_time_budget:
-
-        :return: None
         """
         if self.done_training:
             return
@@ -139,45 +197,79 @@ class Model(object):
         else:
             x_train = clean_en_text(x_train)
 
-        x_train, tokenizer = vectorize_data(x_train)
-        model = LinearSVC(random_state=0, tol=1e-5)
-        model.fit(x_train, ohe2cat(y_train))
+        x_train, word_index, num_features, tokenizer, max_length = sequentialize_data(x_train)
+        num_classes = self.metadata['class_num']
 
-        with open(self.train_output_path + 'model.pickle', 'wb') as handle:
-            pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
+        # initialize model
+        self.model = sep_cnn_model(input_shape=x_train.shape[1:][0],
+                              num_classes=num_classes,
+                              num_features=num_features,
+                              blocks=2,
+                              filters=64,
+                              kernel_size=4,
+                              dropout_rate=0.25)
+        if num_classes == 2:
+            loss = 'binary_crossentropy'
+        else:
+            loss = 'sparse_categorical_crossentropy'
+        optimizer = tf.keras.optimizers.Adam(lr=0.0005)
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=['acc'])
+        callbacks = [tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss', patience=5)]
+        history = self.model.fit(
+            x_train,
+            ohe2cat(y_train),
+            epochs=100,
+            callbacks=callbacks,
+            validation_split=0.1,
+            verbose=2,  # Logs once per epoch.
+            batch_size=16,
+            shuffle=True)
+        print(str(type(x_train)) + " " + str(y_train.shape))
+        self.model.save(self.train_output_path + 'model.h5')
         with open(self.train_output_path + 'tokenizer.pickle', 'wb') as handle:
             pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(self.train_output_path + 'model.config', 'wb') as f:
+            f.write(str(max_length).encode())
+            f.close()
 
         self.done_training=True
 
-
     def test(self, x_test, remaining_time_budget=None):
         """
-        :param x_test: list of str, input test sentence.
+        :param x_test: list of str, input test sentences.
         :param remaining_time_budget:
         :return: A `numpy.ndarray` matrix of shape (sample_count, class_num).
                  here `sample_count` is the number of examples in this dataset as test
                  set and `class_num` is the same as the class_num in metadata. The
                  values should be binary or in the interval [0,1].
         """
-        with open(self.test_input_path + 'model.pickle', 'rb') as handle:
-            model = pickle.load(handle, encoding='iso-8859-1')
+        if self.model is None:
+            self.model = models.load_model(self.test_input_path + 'model.h5')
         with open(self.test_input_path + 'tokenizer.pickle', 'rb') as handle:
             tokenizer = pickle.load(handle, encoding='iso-8859-1')
+        with open(self.test_input_path + 'model.config', 'r') as f:
+            max_length = int(f.read().strip())
+            f.close()
 
         train_num, test_num = self.metadata['train_num'], self.metadata['test_num']
         class_num = self.metadata['class_num']
 
         # tokenizing Chinese words
         if self.metadata['language'] == 'ZH':
+            x_test = clean_zh_text(x_test)
             x_test = list(map(_tokenize_chinese_words, x_test))
+        else:
+            x_test = clean_en_text(x_test)
 
-        x_test = tokenizer.transform(x_test)
-        result = model.predict(x_test)
+        x_test = tokenizer.texts_to_sequences(x_test)
+        x_test = sequence.pad_sequences(x_test, maxlen=max_length)
+        result = self.model.predict_classes(x_test)
 
         # category class list to sparse class list of lists
         y_test = np.zeros([test_num, class_num])
         for idx, y in enumerate(result):
             y_test[idx][y] = 1
         return y_test
+
+
